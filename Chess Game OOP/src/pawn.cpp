65e004d08f2d0d43c3ccfb23b5f3d58364e0f894
Ahmad Raza <ar19002223@gmail.com>
2026-05-02 19:51:50 +0500

    Refactor pawn class and update move logic

--- Chess Game OOP/src/pawn.cpp
+++ Chess Game OOP/src/pawn.cpp
@@ -1,40 +1,48 @@
-#include "Pieces.h"
-pawn::pawn()
+#include "Pawn.h"
+#include <cmath>
+Pawn::Pawn() : Piece()
 {
 	this->moved_before = false;
 }
-pawn::pawn(char color, int row, int col) : piece(color, row, col, 'P')
+Pawn::Pawn(char color, int row, int col) : Piece(color, row, col, 'P')
 {
 	this->moved_before = false;
 }
-void pawn::set_moved_before()
+void Pawn::set_moved_before()
 {
 	this->moved_before = true;
 }
-bool pawn::valid_move(int row, int col, piece* board[8][8]) // to row , to column
+bool Pawn::valid_move(int row, int col, Piece* board[8][8])
 {
 	int from_row = get_row_num();
 	int from_column = get_column_num();
 	char color = get_colour();
-	int direction = 0;
-	if (direction == 'W') // if white then moves up row decrease and vice versa
+
+	int direction;
+	if (color == 'W')
 	{
-		direction = -1;
+		direction = -1; // White moves up (decreasing row index)
 	}
 	else
-		direction = 1;
-	// Move one square
-	if (from_column == col && row == from_column + direction && board[row][col] == nullptr)
+	{
+		direction = 1;  // Black moves down (increasing row index)
+	}
+	// Move one square forward
+	if (from_column == col && row == from_row + direction && board[row][col] == nullptr)
+	{
 		return true;
-	// Move two squares
-	if (col == from_column && row == from_row + 2 * direction && board[from_row + direction][from_column] == nullptr
-		&& board[row][col] == nullptr
-		)
+	}
+	// Move two squares forward
+	if (!moved_before && col == from_column && row == from_row + 2 * direction &&  // pawn can only jum 2 squares at its first moves
+		board[from_row + direction][from_column] == nullptr && board[row][col] == nullptr)
+	{
 		return true;
-	// diagonally
+	}
+	// Diagonal capture
 	if (row == from_row + direction && abs(col - from_column) == 1 &&
-		board[row][col] == nullptr && board[row][col]->get_colour() != get_colour()) // can cpatue enemy piece
+		board[row][col] != nullptr && board[row][col]->get_colour() != color)
+	{
 		return true;
+	}
 	return false;
 }
-
