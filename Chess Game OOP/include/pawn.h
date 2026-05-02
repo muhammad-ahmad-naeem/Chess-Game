25ebe14a614ded80064788779e8731a10be44b4f
Ahmad Raza <ar19002223@gmail.com>
2026-05-02 19:50:43 +0500

    Refactor pawn class to use proper casing and includes

--- Chess Game OOP/include/pawn.h
+++ Chess Game OOP/include/pawn.h
@@ -1,13 +1,11 @@
 #pragma once
-#include "piece.h"
-#include <cmath>
-using namespace std;
-class pawn : public piece
+#include "Piece.h"
+class Pawn : public Piece
 {
 	bool moved_before;
 public:
-	pawn();
-	pawn(char color, int row, int col);
+	Pawn();
+	Pawn(char color, int row, int col);
 	void set_moved_before();
-	bool valid_move(int row, int col, piece* board[8][8]) override;
+	bool valid_move(int row, int col, Piece* board[8][8]) override;
 };
