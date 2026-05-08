eed1461d63e103a92bc329697fe1a5f3aa9d3e24
Ahmad Raza <ar19002223@gmail.com>
2026-05-08 18:27:08 +0500

    Refactor get_input to improve user prompts

--- Chess Game OOP/src/Player.cpp
+++ Chess Game OOP/src/Player.cpp
@@ -12,13 +12,13 @@ char   Player::get_color() const
 {
     return color; 
 }
-bool Player::get_input(string& from_str, string& to_str) const 
+bool Player::get_input(string& from_str, string& to_str) const // helps in game class to update the string directly using &
 {
     if (color == 'W')
         cout << "White";
     else
         cout << "Black";
-    cout << " (" << name << ") enter move" << " (e.g. e2 e4) or 'quit': ";
+    cout << "Enter the current position of the piece: ";
     cin >> from_str;
     // Let Game.h handle quit — just pass it through
     if (from_str == "quit") 
@@ -26,6 +26,7 @@ bool Player::get_input(string& from_str, string& to_str) const
         to_str = "";
         return true;
     }
+    cout << "Enter the Destination by looking at the board: ";
     cin >> to_str;
     // Basic length check
     if (from_str.length() != 2 || to_str.length() != 2) 
