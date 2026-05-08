a5a3ceb07cb0ac94f2d434e277205e828295103b
Ahmad Naeem <ahmadnaeem.offcl@gmail.com>
2026-05-08 10:37:15 +0500

    Enhance Board class with additional functionality
    Added methods for piece retrieval, move validation, check detection, and legal move checking.

--- Chess Game OOP/include/board.h
+++ Chess Game OOP/include/board.h
@@ -1,11 +1,29 @@
 #pragma once
-#include "piece.h"
+#include "Piece.h"
+#include "Bishop.h"
+#include "Rook.h"
+#include "Knight.h"
+#include "Queen.h"
+#include "King.h"
+#include "Pawn.h"
 
 class Board
 {
-protected:
 	Piece* Grid[8][8];
 public:
 	Board();
-	void displayboard();
+	~Board();
+
+	Piece* get_piece(int row, int col) const;
+
+	// Returns true if the move was legal and executed
+	bool make_move(int from_row, int from_col, int to_row, int to_col, char current_turn);
+
+	// Returns true if 'color' king is currently in check
+	bool is_in_check(char color) const;
+
+	// Returns true if 'color' has no legal move (checkmate or stalemate)
+	bool has_no_legal_moves(char color);
+
+	void display() const;
 };
