d98939cc0a5f24e97a78f3acea0104d0c89db6de
Ahmad Raza <ar19002223@gmail.com>
2026-05-08 13:44:01 +0500

    Add Player class to Game for player management

--- Chess Game OOP/include/game.h
+++ Chess Game OOP/include/game.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "Board.h"
-
+#include "Player.h"
 class Game
 {
 	Board board;
+	Player player1;    // White player
+	Player player2;    // Black player
 	char  current_turn;   // 'W' or 'B'
 	bool  game_over;
 
