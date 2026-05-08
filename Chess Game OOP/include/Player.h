852c310e2b43bc63a1f9f82455706d9c69ec1e09
Ahmad Raza <ar19002223@gmail.com>
2026-05-08 16:53:11 +0500

    Define Player class with attributes and methods

--- Chess Game OOP/include/Player.h
+++ Chess Game OOP/include/Player.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Player 
+{
+    string name;
+    char  color;    // 'W' or 'B'
+public:
+    Player(string name, char color);
+    string get_name()  const;
+    char   get_color() const;
+    bool get_input(string& from_str, string& to_str) const;
+};
 
